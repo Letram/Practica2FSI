76a77a298fd3aa05e3880ad3c080684c8733e4b7
Letram <carlos.martel105@alu.ulpgc.es>
2017-03-24 20:18:30 +0000

    Práctica 2.1 FSI
    Primera parte de la práctica 2 FSI en la ULPGC.

--- nn_iris.py
+++ nn_iris.py
@@ -20,8 +20,18 @@ def one_hot(x, n):
 
 data = np.genfromtxt('iris.data', delimiter=",")  # iris.data file loading
 np.random.shuffle(data)  # we shuffle the data
+
 x_data = data[:, 0:4].astype('f4')  # the samples are the four first rows of data
 y_data = one_hot(data[:, 4].astype(int), 3)  # the labels are in the last row. Then we encode them in one hot code
+
+x_data_train = x_data[:105]
+y_data_train = y_data[:105]
+
+x_data_valid = x_data[105:128]
+y_data_valid = y_data[105:128]
+
+x_data_test = x_data[128:]
+y_data_test = y_data[128:]
 
 print "\nSome samples..."
 for i in range(20):
@@ -31,12 +41,19 @@ print
 x = tf.placeholder("float", [None, 4])  # samples
 y_ = tf.placeholder("float", [None, 3])  # labels
 
+# input -> inner nn layer
+
+# 4 entries and 5 neurons
 W1 = tf.Variable(np.float32(np.random.rand(4, 5)) * 0.1)
 b1 = tf.Variable(np.float32(np.random.rand(5)) * 0.1)
 
+# inner nn layer -> outter nn layer
+
+# 5 entries and 3 neurons
 W2 = tf.Variable(np.float32(np.random.rand(5, 3)) * 0.1)
 b2 = tf.Variable(np.float32(np.random.rand(3)) * 0.1)
 
+# outter nn layer -> output
 h = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
 # h = tf.matmul(x, W1) + b1  # Try this!
 y = tf.nn.softmax(tf.matmul(h, W2) + b2)
@@ -56,14 +73,28 @@ print "----------------------"
 
 batch_size = 20
 
-for epoch in xrange(100):
-    for jj in xrange(len(x_data) / batch_size):
-        batch_xs = x_data[jj * batch_size: jj * batch_size + batch_size]
-        batch_ys = y_data[jj * batch_size: jj * batch_size + batch_size]
-        sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+if __name__ == '__main__':
+    for epoch in xrange(100):
+        for jj in xrange(len(x_data_train) / batch_size):
+            # Training zone
+            batch_xs = x_data[jj * batch_size: jj * batch_size + batch_size]
+            batch_ys = y_data[jj * batch_size: jj * batch_size + batch_size]
+            sess.run(train, feed_dict={x: x_data_train, y_: y_data_train})
+            # End of training zone
 
-    print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys})
-    result = sess.run(y, feed_dict={x: batch_xs})
-    for b, r in zip(batch_ys, result):
+        # Validation zone
+        print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: x_data_valid, y_: y_data_valid})
+        result = sess.run(y, feed_dict={x: x_data_valid})
+        for b, r in zip(y_data_valid, result):
+            print b, "-->", r
+        print "----------------------------------------------------------------------------------"
+        # End of validation zone
+
+    # Once we are done validating our nn we test it using the other array counting how many errors it makes.
+    error = 0
+    for b, r in zip(y_data_test, result):
+        if np.argmax(b) != np.argmax(r):
+            error += 1
         print b, "-->", r
     print "----------------------------------------------------------------------------------"
+    print "Error:", error
